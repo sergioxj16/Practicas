586aa8ad354214d9299d0de17262b1bf23e03d9e
jingyi <jingyixj@gmail.com>
2024-11-11 09:18:26 +0100

    Quitar carpeta pruebas

--- Pruebas/npmExample/index.js
+++
@@ -1,4 +0,0 @@
-import dayjs from "dayjs"
-import "./node_modules/dayjs/dayjs.min.js"
-
-console.log(dayjs('2023-10-25').format('DD/MM/YYYY'));
--- Pruebas/utils/utils.js
+++
@@ -1,4 +0,0 @@
-export function convertBase64(file) {
-    return new Promise((resolve, reject))
-    
-}
